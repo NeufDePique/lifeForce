6adcf1e37f51c4f59fef53fcf0828ba87568bfb9
NeufDePique <claire.marche@orange.fr>
2017-04-06 14:58:27 +0200

    Declared general variables

--- lifeForce.js
+++ lifeForce.js
@@ -1,1 +1,16 @@
-//
+window.onload = init();
+
+final LVL_MAX;
+final PRIMARY_AXE_LEN;
+final SECONDARY_AXE_LEN;
+final SCROLLING_SPEED;
+final WIDTH;
+final HEIGHT;
+
+var score;
+var lives;
+var level;
+var level_orientation;
+var eligible_power_up;
+var lvl_size = [];
+var current_map_progression;
