84fe13ed4201b46e95ac9dbccbd46f830b3909bb
NeufDePique <claire.marche@orange.fr>
2017-04-13 13:41:15 +0200

    added arrows

--- lifeForce.js
+++ lifeForce.js
@@ -1,9 +1,9 @@
 const LVL_MAX = 1;
-const PRIMARY_AXE_LEN = 42;
-const SECONDARY_AXE_LEN = 42;
-const SCROLLING_SPEED = 42;
-const WIDTH = 42;
-const HEIGHT = 42;
+const PRIMARY_AXE_LEN = 10000;
+const SECONDARY_AXE_LEN = 500000;
+const SCROLLING_SPEED = 4;
+const WIDTH = 800;
+const HEIGHT = 600;
 
 var ctx = null;
 
@@ -14,6 +14,8 @@ var level_orientation;
 var eligible_power_up;
 var lvl_size = [];
 var current_map_progression;
+
+var arrows = {up: false, right: false, down: false, left: false};
 
 var rect = {x: 0, y: 0, width: 0, height: 0};
 var power_ups = [];
@@ -36,7 +38,7 @@ init = function() {
 		bullet = {rect: {}, exists: false, power: 0, speed: 0, friend_bul: false};
 		bullets[i] = bullet;
 	}
-	space_ship.rect = {x: 0, y: ctx.height, width: 40, height: 20};
+	space_ship.rect = {x: 0, y: ctx.height / 2, width: 40, height: 20};
 	game();
 }
 
