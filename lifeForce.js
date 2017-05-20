5b8ac453fe5378c27447887ba9e1975290fd3f70
NeufDePique <claire.marche@orange.fr>
2017-05-20 23:22:21 +0200

    zevtfszefbsefhbdjzebgjqdnffjbfeeqjnfdhnnjqdjknjng

--- lifeForce.js
+++ lifeForce.js
@@ -40,7 +40,8 @@ var foe = {rect: {}, type: ' ', shooting: false, weapon: {}, has_powup: false, s
 var foes = [[]];
 var space_ship = {rect: {}, speed: 0, weapon: {}, lvl_weapon: 0, missile: false, ripple: false, laser: false, force: false, invulnerability: false, invulnerability_timer: 0};
 var wall = {rect: {}, type: ' ', destroyable: false};
-var walls = [[]];
+var walls = [[[]]];
+var min_wall_on_screen_index = [];
 
 init = function() {
 	
@@ -225,20 +226,33 @@ update = function(d) {
 	}
 	
 	for (i = 0; i <= walls[level].length - 1; i++) {
-		for (j = 0; j <= bullets.length - 1; j++) {
-			if (bullets[j].exists && collides(bullets[j], walls[level][i])) {
-				reset_bullet(bullets[j]);
-				if (walls[level][i].destroyable) {
-					walls[level][i].rect.width = 0;
-					walls[level][i].rect.height = 0;
+		while (pos_on_map > walls[level][i][min_wall_on_screen_index[i]].x + walls[level][i][min_wall_on_screen_index[i]].width) {
+			min_wall_on_screen_index[i]++;
+		}
+		
+		for (j = min_wall_on_screen_index[i]; j <= walls[level][i].length - 1; j++) {
+			for (k = 0; k <= bullets.length - 1; k++) {
+				if (bullets[k].exists && collides(bullets[k], walls[level][i][j])) {
+					reset_bullet(bullets[j]);
+					if (walls[level][i][j].destroyable) {
+						walls[level][i][j].rect.width = 0;
+						walls[level][i][j].rect.height = 0;
+					}
 				}
 			}
+			
+			if (collidesWall(space_ship, walls) && !space_ship.invulnerability) {
+				lifeLost();
+			}
+			
+			if (!entity_on_screen(walls[level][i][j])) {
+				break;
+			}
 		}
+	}
 		if (!(space_ship.invulnerability) && collides(space_ship, walls[level][i])) {
 			life_lost();
 		}
-	}
-	
 	if (space_ship.invulnerability_timer > 0) {
 		space_ship.invulnerability_timer--;
 	}
@@ -334,11 +348,13 @@ render = function() {
 
 lvl_create = function() {
 	for (i = 0; i <= lvl_maps[level].length - 1; i++) {
+		min_wall_on_screen_index.push(0);
+		walls[level].push([]);
 		for (j = 0; j <= lvl_maps[level][i].length - 1; j++) {
 			if ("WwB".indexOf(lvl_maps[level][i].charAt(j)) > -1) {
 				wall = {rect: {x: j * UNIT_MAP, y: i * UNIT_MAP, width: UNIT_MAP, height: UNIT_MAP}, type: lvl_maps[level][i].charAt(j)};
 				wall.destroyable = (lvl_maps[level][i].charAt(j).toLowercase == lvl_maps[level][i].charAt(j));
-				walls[level].push(wall);
+				walls[level][i].push(wall);
 			}
 		}
 	}
@@ -349,9 +365,20 @@ collides = function(a, b) {
 }
 
 collidesWall = function(entity, walls) {
-	for (i = 0; i <= walls.length - 1; i++) {
-		if (collides(entity, walls[i])) {
-			return true;
+	
+	for (i = 0; i <= walls[level].length - 1; i++) {
+		while (pos_on_map > walls[level][i][min_wall_on_screen_index[i]].x + walls[level][i][min_wall_on_screen_index[i]].width) {
+			min_wall_on_screen_index[i]++;
+		}
+		
+		for (j = min_wall_on_screen_index[i]; j <= walls[level][i].length - 1; j++) {
+			if (collides(entity, walls[level][i][j])) {
+				return true;
+			}
+			
+			if (!entity_on_screen(walls[level][i][j])) {
+				break;
+			}
 		}
 	}
 	return false;
